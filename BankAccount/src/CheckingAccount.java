9280a36251446d144064f5a4ffefa6048dd1331a
daviddimmit <daviddimmit@localhost>
2024-06-15 23:09:40 -0600

    keep updating

--- BankAccount/src/CheckingAccount.java
+++ BankAccount/src/CheckingAccount.java
@@ -1,22 +1,24 @@
 
 public class CheckingAccount extends BankAccount {
-//	Has an interest rate attribute
+//	will store interest rate and count overdrafts
 	protected double interestRate;
+	protected int overdrafts;
 //	Allows overdraft withdrawals and charges a $30 fee
 	public CheckingAccount() {
 		super();
 		this.interestRate = 0;
+		this.overdrafts = 0;
 	}
 //	Methods:
 	public void setInterestRate(double rate) {
 		this.interestRate = rate;
 	}
-//
 //	processWithdrawal() - will display a negative balance that includes a $30 overdraft fee and denotes that a fee has been accessed
 	public void processWithdrawal(double withdrawal) {
 		if (this.balance - withdrawal < 0) {
 			super.withdrawal(withdrawal + 30.0);  // go thru with withdrawal adding 30 for the overdraft charge
 			System.out.println("30 dollar overdraft fee charged.");
+			this.overdrafts = this.overdrafts + 1;
 		}  else {
 			super.withdrawal(withdrawal);
 		}
@@ -26,6 +28,7 @@ public class CheckingAccount extends BankAccount {
 		System.out.println(this.firstName + " " + this.lastName + "'s Checking Account");
 		System.out.println("Account ID: " + this.accountID);
 		System.out.println("Balance: $" + this.balance);
-		System.out.println("Interest Rate: " + interestRate);
+		System.out.println("Interest Rate: " + this.interestRate);
+		System.out.println("Overdrafts: " + this.overdrafts);
 	}
 }
